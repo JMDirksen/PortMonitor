990b93cf26863155d05acff5621b2dbe6d7cfd4c
JMDirksen <jeftadirksen@gmail.com>
2023-12-21 19:54:36 +0100

    structured

--- run.sh
+++ run.sh
@@ -2,10 +2,17 @@
 
 while :
 do
-  for port in $PORT_LIST; do
+  for port in $PORT_LIST
+  do
     echo -n "> $port ... "
     nc -zw $TIMEOUT $(echo $port | tr ":" " ")
-    [[ $? -eq 0 ]] && echo "OK" || (echo "Error"; curl -s -o /dev/null -d "Error connecting to $port" ntfy.sh/$NTFY_TOPIC)
+    if [ $? -eq 0 ]
+    then
+      echo "OK"
+    else
+      echo "Error"
+      curl -s -o /dev/null -d "Error connecting to $port" ntfy.sh/$NTFY_TOPIC
+    fi
   done
   sleep $INTERVAL
 done
