8d3a033fcaaff9ab7a21b1accd4a4b9a1eb411df
JMDirksen <jeftadirksen@gmail.com>
2024-02-07 07:57:23 +0100

    vars test, refactor

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-while :
-do
-  for port in $PORT_LIST
-  do
+if [ -z ${PORT_LIST} ]; then PORT_LIST="example.com:80 example.com:443"; fi
+if [ -z ${TIMEOUT} ]; then TIMEOUT=3; fi
+if [ -z ${INTERVAL} ]; then INTERVAL=60; fi
+if [ -z ${NTFY_TOPIC} ]; then NTFY_TOPIC="PortMonitor"; fi
+
+while true; do
+  for port in $PORT_LIST; do
     echo -n "> $port ... "
     nc -zw $TIMEOUT $(echo $port | tr ":" " ")
-    if [ $? -eq 0 ]
-    then
+    if [ $? -eq 0 ]; then
       echo "OK"
     else
       echo "Error"
