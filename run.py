c34f13b17356411f882654beb5053b9cb349b06a
JMDirksen <jeftadirksen@gmail.com>
2024-02-14 10:23:36 +0000

    Removed INTERVAL_ON_ERROR

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import socket
 import requests
 import time
 
+# Get/set environment variables / defaults
 PORT_LIST = os.getenv('PORT_LIST') or "example.com:80 example.com:443"
 INTERVAL = int(os.getenv('INTERVAL') or 300)
-INTERVAL_ON_ERROR = int(os.getenv('INTERVAL_ON_ERROR') or 10)
 NOTIFY_ERROR_COUNT = int(os.getenv('NOTIFY_ERROR_COUNT') or 2)
 TIMEOUT = int(os.getenv('TIMEOUT') or 3)
 NTFY_TOPIC = os.getenv('NTFY_TOPIC') or "PortMonitor"
@@ -15,24 +15,24 @@ def main():
     ports = ports_to_list(PORT_LIST)
 
     while True:
-        errors = False
         for port in ports:
             print(f"> {port['string']} ... ", end="", flush=True)
+            # Check port
             if checkPort(port['address'], port['port']):
+                # OK
                 print("OK", flush=True)
                 if port['error_count'] >= NOTIFY_ERROR_COUNT:
+                    # Notify back from Error to OK
                     send_notification("OK", port['string'])
                 port['error_count'] = 0
             else:
-                errors = True
+                # Error
                 port['error_count'] += 1
                 print(f"ERROR {port['error_count']}", flush=True)
                 if port['error_count'] == NOTIFY_ERROR_COUNT:
+                    # Notify Error
                     send_notification("Error", port['string'], True)
-        if errors:
-            time.sleep(INTERVAL_ON_ERROR)
-        else:
-            time.sleep(INTERVAL)
+        time.sleep(INTERVAL)
 
 
 def send_notification(title: str, message: str, warning: bool = False):
